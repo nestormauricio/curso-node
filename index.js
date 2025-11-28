3e0b397d1f8f97f2fa68ca3c7bbd8a722c1ed3f7
Nestor Mauricio Pérez Ruíz <nestormauricio@live.com>
2025-11-28 07:20:06 -0500

    Primer comit ya iniciado Express

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+app.post('/saludo', (req, res) => {
+  const { nombre } = req.body || {};
+
+  if (!nombre) {
+    return res.status(400).json({ error: 'El campo nombre es obligatorio' });
+  }
+
+  res.json({ mensaje: `Hola, ${nombre}` });
+});
+
+
+app.listen(port, () => {
+  console.log(`API corriendo en http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Este funcionó pero había que enviar el json {"nombre": "Mauricio"}
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
+// const port = 3000;
+
+// app.get('/ping', (req, res) => {
+//   res.json({ message: 'pong' });
+// });
+
+// app.post('/saludo', (req, res) => {
+//   const { nombre } = req.body;
+//   res.json({ mensaje: `Hola, ${nombre}` });
+// });
+
+// app.listen(port, () => {
+//   console.log(`API corriendo en http://localhost:${port}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
+// const port = 3000;
+
+// app.get('/ping', (req, res) => {
+//   res.json({ message: 'pong' });
+// });
+
+// app.listen(port, () => {
+//   console.log(`API corriendo en http://localhost:${port}`);
+// });
+
+// app.post('/saludo', (req, res) => {
+//   const { nombre } = req.body;
+//   res.json({ mensaje: `Hola, ${nombre}` });
+// });
